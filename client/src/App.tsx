09a0cba30768cb980ddd335fbb18a2551982d915
Esteban Helguero <esteban.helguero@s4rb.com>
2023-10-29 03:51:17 +0000

    Added searchbar, loader and weatherforecast to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import WeatherForecast from './components/WeatherForecast';
+import SearchBar from './components/SearchBar';
+import useWeatherData from './hooks/useWeatherData';
+import { useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const {
+    weatherData,
+    searchOptions,
+    searchTerm,
+    isLoading,
+    handleOptionSelect,
+    handleFormSubmit,
+    handleInputChange,
+    handleOnClear,
+    getActualPosition,
+  } = useWeatherData();
+
+  useEffect(() => {
+    getActualPosition();
+  }, [getActualPosition]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex justify-center bg-gradient-to-br from-sky-900 via-cyan-800 to-cyan-800 min-h-[100vh] w-full">
+      <div className="w-full md:max-w-[1024px] py-4 md:py-4 md:px-10 lg:px-24 h-full lg:h-auto backdrop-blur-ls rounded drop-shadow-lg">
+        <SearchBar
+          term={searchTerm}
+          options={searchOptions}
+          onInputChange={handleInputChange}
+          onOptionSelect={handleOptionSelect}
+          onSubmit={handleFormSubmit}
+          onClear={handleOnClear}
+          placeholder="Add city"
+        />
+        {isLoading ? (
+          <div className="w-full h-screen flex items-center justify-center">
+            <div className="loader ease-linear rounded-full border-t-4 border-gray-200 h-12 w-12 animate-spin"></div>
+          </div>
+        ) : (
+          weatherData && <WeatherForecast data={weatherData} />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
