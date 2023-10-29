6afef6d3a678f70f56f6a8ee005b9b61d9e8fada
Esteban Helguero <esteban.helguero@s4rb.com>
2023-10-29 01:35:43 +0100

    Added tailwind. Updated jest config and types

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,7 +1,29 @@
 export default {
   testEnvironment: 'jsdom',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
+              options: {
+                metaObjectReplacement: {
+                  env: {
+                    VITE_OPENWEATHER_API_URL: 'https://www.url.com',
+                    VITE_OPENWEATHER_API_KEY: '1234',
+                  },
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
   },
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
