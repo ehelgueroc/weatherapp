76ea9a008a3cd84b69955bab0a9133c05311000a
Esteban Helguero <esteban.helguero@canda.com>
2024-12-20 13:58:34 +0000

    fix test

--- api/src/main.ts
+++ api/src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Weather API')
     .setDescription(
-      'Weather API to search for cities and get forecast for each',
+      'Weather API to search for cities and get forecast for each one of them',
     )
     .setVersion('1.0')
     .build();
